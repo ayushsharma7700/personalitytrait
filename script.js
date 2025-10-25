622a5145cb76f840bf44fccb68f58bc01a2c7f02
ayushsharma7700 <ayushmps111@gmail.com>
2025-10-25 22:51:50 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,21 @@
-// Import Firebase SDK functions
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Paste your config here 👇
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSy...yourKey...",
-  authDomain: "your-app.firebaseapp.com",
-  projectId: "your-app",
-  storageBucket: "your-app.appspot.com",
-  messagingSenderId: "1234567890",
-  appId: "1:1234567890:web:abcdef123456"
+  apiKey: "AIzaSyCpFfZG2wTUidK06-bdaw9aqBQQBO4xMqc",
+  authDomain: "personality-rating.firebaseapp.com",
+  projectId: "personality-rating",
+  storageBucket: "personality-rating.firebasestorage.app",
+  messagingSenderId: "1056348504143",
+  appId: "1:1056348504143:web:e48f5b49bedfacb3b7ea3a",
+  measurementId: "G-VGN3DFRCQD"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
-
-export { db };
+const analytics = getAnalytics(app);
